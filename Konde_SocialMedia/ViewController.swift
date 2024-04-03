d05d28795357d184b44e8f61004a21352286e7bf
NickodemusK <ntkonde88@gmail.com>
2024-04-03 11:35:41 -0400

    Adding a test print function.

--- Konde_SocialMedia/ViewController.swift
+++ Konde_SocialMedia/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func testPrint(){
+        print("Hi")
+    }
 
 
 }
